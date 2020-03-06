0acbdbe169ca9c4c469341a12b95e3289afb63ae
Ilya <viziter.1994@mail.ru>
2020-03-06 16:05:21 +0300

    feat: completes test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+     if (n >= 0) {
+        n = n + ' ';
+    return +n.split('').reverse().join('');
+     }
+        else {
+            n = Math.abs(n);
+             n = n + ' ';
+     return +n.split('').reverse().join('');
+        }
 }
+
+
